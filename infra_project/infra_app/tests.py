52dcdb09c68afc488170012eed661f79b8505aa7
Evgeny <evgeniydudkin@yandex.ru>
2021-05-21 00:07:15 +0300

    sprint19-2

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 from django.test import Client, TestCase
 
+
 class StaticPagesURLTests(TestCase):
     def setUp(self):
         self.guest_client = Client()
@@ -17,7 +18,6 @@ class StaticPagesURLTests(TestCase):
         """Проверка контекста страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
-
 
         response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
