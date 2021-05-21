356a000392be164e0c5772ab6f04ffe4c04e53e9
Evgeny <evgeniydudkin@yandex.ru>
2021-05-21 22:24:38 +0300

    sprint19-deploy

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "84.201.168.39",
+    "evgeniykolesnikov.ru",
+    "www.evgeniykolesnikov.ru"
+]
 
 
 # Application definition
